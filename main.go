bce2eb056dbf782d8a304ec18336b856c45347c8
pedramcode <dev.dehghanpour@gmail.com>
2023-04-15 15:30:51 +0330

    Middlewares

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/pedramcode/goblog/core"
 	"github.com/pedramcode/goblog/routers"
 )
@@ -12,7 +13,12 @@ func main() {
 
 	server := echo.New()
 	apiGroup := server.Group("/api")
+
 	routers.UserRouter(apiGroup)
 	routers.PostRouter(apiGroup)
+
+	server.Use(middleware.Logger())
+	server.Use(middleware.Recover())
+	server.Use(middleware.CORS())
 	server.Logger.Fatal(server.Start(":8080"))
 }
