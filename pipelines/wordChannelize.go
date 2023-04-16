fca1577c82f143049b00bc3133b125c1ca462eaf
pedramcode <dev.dehghanpour@gmail.com>
2023-04-16 13:00:09 +0330

    Pipelines

--- pipelines/wordChannelize.go
+++ pipelines/wordChannelize.go
@@ -3,7 +3,15 @@ package pipelines
 import "strings"
 
 func WordChannelize(data string) <-chan string {
-	words := strings.Split(data, " ")
+	wordsRep := strings.Replace(data, "!", " ", -1)
+	wordsRep = strings.Replace(data, "?", " ", -1)
+	wordsRep = strings.Replace(data, ".", " ", -1)
+	wordsRep = strings.Replace(data, "(", " ", -1)
+	wordsRep = strings.Replace(data, ")", " ", -1)
+	wordsRep = strings.Replace(data, ":", " ", -1)
+	wordsRep = strings.Replace(data, ";", " ", -1)
+	wordsRep = strings.Replace(data, "&", " ", -1)
+	words := strings.Split(wordsRep, " ")
 	channel := make(chan string)
 	go func() {
 		for _, word := range words {
